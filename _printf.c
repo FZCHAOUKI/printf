cea9cfa6a21e77018f8276a190e1789335c95df9
FZCHAOUKI <fatimazahrae.chaouki@gmail.com>
2023-07-04 11:46:34 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,40 +1,42 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
-
+#include <stdarg.h>
 
 /**
- * _printf - Produces output according to a format.
- * @format: Format string.
+ * _printf - Printf function
+ * @format: format string.
  *
- * Return: Number of characters printed (excluding the null byte).
+ * Return: Number of characters printed (excluding null byte).
  */
-
-#include <stdio.h>
-#include <stdarg.h>
-
 int _printf(const char *format, ...)
 {
 	int printed_chars = 0;
 	va_list args;
 
+	if (format == NULL)
+		return (-1);
+
 	va_start(args, format);
 
 	while (*format)
 	{
-		if (*format == '%')
+		if (*format != '%')
+		{
+			putchar(*format);
+			printed_chars++;
+		}
+		else
 		{
 			format++;
 			if (*format == '\0')
 				break;
-			else if (*format == '%')
+			if (*format == '%')
 			{
 				putchar('%');
 				printed_chars++;
 			}
 			else if (*format == 'c')
 			{
-				int c = va_arg(args, int);
+				char c = va_arg(args, int);
 				putchar(c);
 				printed_chars++;
 			}
@@ -48,11 +50,12 @@ int _printf(const char *format, ...)
 					printed_chars++;
 				}
 			}
-		}
-		else
-		{
-			putchar(*format);
-			printed_chars++;
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+				printf("%d", num);
+				printed_chars += count_digits(num);
+			}
 		}
 
 		format++;
@@ -61,4 +64,26 @@ int _printf(const char *format, ...)
 	va_end(args);
 
 	return (printed_chars);
+}
+
+/**
+ * count_digits - Count the number of digits in an integer.
+ * @num: The integer to count digits.
+ *
+ * Return: Number of digits.
+ */
+int count_digits(int num)
+{
+	int count = 0;
+
+	if (num == 0)
+		return (1);
+
+	while (num != 0)
+	{
+		num /= 10;
+		count++;
+	}
+
+	return (count);
 }
